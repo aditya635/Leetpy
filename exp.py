fee77461d3ed190c499586b993f48f129f2c04ff
aditya635 <55912563+aditya635@users.noreply.github.com>
2022-02-12 17:47:26 +0530

    50% done

--- exp.py
+++ exp.py
@@ -1,4 +1,8 @@
 import json
+import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 f = open('test.json')
  
@@ -6,5 +10,20 @@ f = open('test.json')
 # a dictionary
 data = json.load(f)
 
+conn = psycopg2.connect(
+    host=os.environ['host'],
+    database=os.environ['database'],
+    user=os.environ['user'],
+    password=os.environ['password'])
+
+
+cursor = conn.cursor()
+
 for leetobj in data["total_rank"]:
     print(f' username:{leetobj["username"]} and rank:{leetobj["rank"]}')
+    query='''INSERT INTO leet(name,rank) VALUES (%s,%s)'''
+    data = (leetobj["username"],leetobj["rank"])
+    cursor.execute(query,data)
+
+conn.commit()
+conn.close()
--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 import requests
 import json
+import math 
+import os
+from time import sleep
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.environ['host'],
+    database=os.environ['database'],
+    user=os.environ['user'],
+    password=os.environ['password'])
+
+
+cursor = conn.cursor()
 
 print ("Please tell which contest number you want :")
 m = int(input())
 text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination=1")
-print(text.json())
-with open ('test.json', 'w') as json_file:
-    json.dump(text.json(), json_file)
+data = text.json()
+pages = math.ceil(data['user_num']/25)
+current = 0
+for i in range(1,pages+1):
+
+    text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination={i}")
+    info = text.json()
+    
+    for leetobj in info["total_rank"]:
+        query='''INSERT INTO leet(name,rank) VALUES (%s,%s)'''
+        data = (leetobj["username"],leetobj["rank"])
+        cursor.execute(query,data)
+    conn.commit()
+    current+=1
+    if( current % 10 == 0):
+        print(f"Well {current} requests has been made")
+        sleep(1)
+    
+
+conn.close()
+
