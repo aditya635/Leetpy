7ccd387a86d036e4d01a41ab89e3a45b53d7fd41
aditya635 <55912563+aditya635@users.noreply.github.com>
2022-02-12 21:59:58 +0530

    fast and without china exclusion

--- exp.py
+++ exp.py
@@ -1,29 +1,12 @@
 import json
-import psycopg2
+import psycopg2,requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-f = open('test.json')
- 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
-
-conn = psycopg2.connect(
-    host=os.environ['host'],
-    database=os.environ['database'],
-    user=os.environ['user'],
-    password=os.environ['password'])
-
-
-cursor = conn.cursor()
-
+data = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-279/?pagination=1&region=india")
+data=data.json()
+print(data)
 for leetobj in data["total_rank"]:
     print(f' username:{leetobj["username"]} and rank:{leetobj["rank"]}')
-    query='''INSERT INTO leet(name,rank) VALUES (%s,%s)'''
-    data = (leetobj["username"],leetobj["rank"])
-    cursor.execute(query,data)
-
-conn.commit()
-conn.close()
+    
--- main.py
+++ main.py
@@ -18,18 +18,17 @@ cursor = conn.cursor()
 
 print ("Please tell which contest number you want :")
 m = int(input())
-text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination=1")
+text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination=1&region=india")
 data = text.json()
 pages = math.ceil(data['user_num']/25)
 current = 0
 for i in range(1,pages+1):
-
-    text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination={i}")
+    text = requests.get(f"https://leetcode.com/contest/api/ranking/weekly-contest-{m}/?pagination={i}&region=india")
     info = text.json()
     
     for leetobj in info["total_rank"]:
         query='''INSERT INTO leet(name,rank) VALUES (%s,%s)'''
-        data = (leetobj["username"],leetobj["rank"])
+        data = (leetobj["username"],leetobj["rank"]+1)
         cursor.execute(query,data)
     conn.commit()
     current+=1
